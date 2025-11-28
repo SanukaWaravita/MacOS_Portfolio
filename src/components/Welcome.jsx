9c628bacb37cdee121ca2c6353fb51d5064aba13
SanukaWaravita <sswaravita@students.nsbm.ac.lk>
2025-11-28 08:31:39 +0530

    Initialised welcome text animation process; func created to separately characters of string to add styling and animation

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,14 +1,33 @@
 import React, { useRef } from 'react'
 
+const renderText = (text, className, baseWeight = 400) => {
+    return [... text].map((char, i) => (
+        <span
+            key={i}
+            className={className}
+            style={{ fontVariationSettings: `'whgt ${baseWeight}`}}
+        >
+            {char === " " ? '\u00A0' : char}
+        </span>
+    ))
+}
+
 const Welcome = () => {
     const titleRef = useRef(null);
     const subtitleRef = useRef(null);
 
   return (
     <section id="welcome">
-        <p ref={subtitleRef}>Hey, I's Gojo! Welcome to my</p>
+        <p ref={subtitleRef}>
+            {renderText("Hey, I's Gojo! Welcome to my", 
+                        'text-3xl font-georama', 
+                        100,
+            )}
+        </p>
         <h1 ref={titleRef} className="mt-7">
-            portfolio
+            {renderText("portfolio", 
+                        'text-9xl italic font-georama'
+            )}
         </h1>
 
         <div className="small-screen">
